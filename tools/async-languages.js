3822aa2a43c45667f18fc36e7b482434208a06f8
Tom MacWright <tmcw@users.noreply.github.com>
2019-04-12 16:03:28 -0700

    Polyglot support & updates (#2)
    * Merge upstream changes
    
    * Dynamic language support
    
    * Add language map
    
    * Adjust package for async languages
    
    * Tag as alpha
    
    * Build AMD
    
    * Make map AMD as well
    
    * alpha.2
    
    * Run required languages
    
    * Register language
    
    * alpha.3

---
+++ tools/async-languages.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+
+try {
+  fs.mkdirSync("./async-languages");
+} catch (e) {}
+
+function getRequires(f) {
+  const requires = f.match(/\nRequires:(.*)/);
+  if (requires) {
+    return requires[1].split(",").map(r => r.trim());
+  }
+  return [];
+}
+
+let languageMap = {};
+
+for (let language of fs.readdirSync("./src/languages")) {
+  let f = fs.readFileSync(`./src/languages/${language}`, "utf8");
+  let requires = getRequires(f);
+  f =
+    f.replace(
+      "function(hljs) {",
+      `define(${JSON.stringify(
+        requires.map(r => "./" + r)
+      )}, (...$$requiredLanguages) => function(hljs) {
+        ${requires
+          .map(
+            (r, i) =>
+              `hljs.registerLanguage(${JSON.stringify(
+                path.basename(r, ".js")
+              )}, $$requiredLanguages[${i}]);`
+          )
+          .join("\n")}
+          
+          `
+    ) + ")";
+
+  let aliases = [path.basename(language, ".js")];
+  const specifiedAliases = f.match(/\baliases: \[([^\]]*)\]/);
+  if (specifiedAliases) {
+    aliases.push(
+      ...specifiedAliases[1].split(",").map(s => s.replace(/'/g, "").trim())
+    );
+  }
+
+  for (let alias of aliases) {
+    languageMap[alias] = language;
+  }
+
+  fs.writeFileSync(`./async-languages/${language}`, f);
+}
+
+fs.writeFileSync(
+  "./async-languages/index.js",
+  `define(() => new Map(${JSON.stringify(Object.entries(languageMap))}))`
+);
